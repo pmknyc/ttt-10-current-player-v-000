17d6d15f67036fcc234eaafa3ae181c248c1b184
Paula McKinley <pmknyc@optimum.net>
2018-08-21 20:35:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,10 +4,9 @@ board = ["O", "X", "X", "O", "X", "O", " ", " ", " "]
 count = 0
 def turn_count(board)
   board.each do |move|
-      if move == "X" || move == "O"
-      count += 1
-      end
-    end
+    move == "X" || move == "O"
+    count += 1
+  end
 end
 
 
